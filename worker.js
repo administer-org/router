ae2afaaba84fd94343d679487b79101b1259467e
Pyx <git@notpyx.me>
2025-03-26 21:59:29 -0400

    add blog shortlink, resort the dict

--- worker.js
+++ worker.js
@@ -4,12 +4,13 @@ export default {
         const fpath = url.pathname.slice(1);
 
         const routes = {
-            "discord":   "https://discord.gg/3Q8xkcBT3M",
+            "":          "https://docs.admsoftware.org/meta/router",
             "git":       `https://github.com/administer-org/${fpath.replace(/^git\//, '')}`,
-            "discourse": "https://devforum.roblox.com/t/3179989",
-            "roblox":    "https://create.roblox.com/store/asset/127698208806211/Administer",
             "docs":      "https://docs.admsoftware.org",
-            "":          "https://docs.admsoftware.org/meta/router"
+            "blog":      "https://blog.admsoftware.org",
+            "roblox":    "https://create.roblox.com/store/asset/127698208806211/Administer",
+            "discord":   "https://discord.gg/3Q8xkcBT3M",
+            "discourse": "https://devforum.roblox.com/t/179989"
         };
 
         for (const [route, path] of Object.entries(routes)) {
